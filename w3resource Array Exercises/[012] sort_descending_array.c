f66f7d3466a69556a995dcfe4298c5aebcbc19d3
Harvey Julian Ermitanio <77192438+harveyjulian@users.noreply.github.com>
2025-12-24 14:08:17 +0800

    Implement array sorting in descending order
    The program sorts an array in descending order using a nested loop for comparison and swapping elements.

---
+++ w3resource Array Exercises/[012] sort_descending_array.c
@@ -0,0 +1,46 @@
+/*
+	Problem: Write a program in C to sort elements of an array in ascending order. 
+	Date: 12/24/2025
+*/
+
+#include <stdio.h>
+
+int
+main(void)
+{
+	int arr_size, array[50], i, j, temp;
+	
+	printf("Enter array size: ");
+	scanf("%d", &arr_size);
+	
+	/* Get all elements */
+	for (i = 0; i < arr_size; i++)
+	{
+		printf("Enter element[%d]: ", i);
+		scanf("%d", &array[i]);
+	}
+	
+	printf("\n");
+	
+	/* Sort array */
+	for (i = 0; i < arr_size; i++)
+	{
+		for (j = i + 1; j < arr_size; j++)
+		{
+				if (array[i] < array[j])
+				{
+					temp = array[i];
+					array[i] = array[j];
+					array[j] = temp;
+				}		
+		}	
+	}
+	
+	printf("\n");
+	printf("Sorted array: ");
+	
+	/* Print array */
+	for (i = 0; i < arr_size; i++)
+		printf("%d ", array[i]);
+	
+}
